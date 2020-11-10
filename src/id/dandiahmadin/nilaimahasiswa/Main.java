9b4df39edee2a7b4f4c98df2c6d1fedf251bcdbc
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-11-10 21:08:20 +0700

    Initial Commit

---
+++ src/id/dandiahmadin/nilaimahasiswa/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dandiahmadin.nilaimahasiswa;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi menentukan nilai mahasiswa dengan pendekatan object oriented
+ */
+public class Main {
+    public static void main(String[] args) {
+        NilaiMahasiswa mhs = new NilaiMahasiswa();
+        mhs.setQuiz(60);
+        mhs.setUts(80);
+        mhs.setUas(75);
+        
+        System.out.println("QUIZ\t\t= " + mhs.getQuiz());
+        System.out.println("UTS\t\t= " + mhs.getUts());
+        System.out.println("UAS\t\t= " + mhs.getUas());
+        
+        mhs.setNilaiAkhir(mhs.hitungNilaiAkhir(mhs.getQuiz(), mhs.getUts(), mhs.getUas()));
+        System.out.println("\nNilai Akhir\t= " + mhs.getNilaiAkhir());
+        
+        mhs.setIndex(mhs.indexNilai(mhs.getNilaiAkhir()));
+        System.out.println("\nIndex\t\t= " + mhs.getIndex());
+        
+        mhs.setKeterangan(mhs.keteranganIndex(mhs.getIndex()));
+        System.out.println("Keterangan\t= ".concat(mhs.getKeterangan()));
+    }
+}
